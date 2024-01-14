1392eda9d003a54b726e548d916d1a2457963fb5
Pratik <pratikparab912@gmail.com>
2024-01-14 12:17:23 +0530

    Docker study material by LCWD

---
+++ java-project/Test.java
@@ -0,0 +1,13 @@
+class Test {
+
+    // public static void printSystemProperties() {
+    //     System.out.println("Printing System properties Using Java");
+    //     Properties properties = System.getProperties();
+    //     System.out.println(properties);
+    // }
+
+    public static void main(String[] args) {
+        System.out.println("Java Program Started...");
+        // printSystemProperties();
+    }
+}
---
+++ python-project/main.py
@@ -0,0 +1,5 @@
+print("This is my Python Project")
+def myFun(m1,m2):
+    print("Sum is : ", (m1+m2))
+    myFun(1,2)
+print("By By Python")
